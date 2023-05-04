efedc49f4d89b952a2f364c6a7206d42893d244d
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-05-04 10:03:28 +0330

    update multiSelect component

--- src/components/multiSelect.tsx
+++ src/components/multiSelect.tsx
@@ -1,72 +1,39 @@
-import { useContext } from 'react';
-import { AppContext } from '../hooks/stateContext';
-import {Form, DropdownButton} from 'react-bootstrap';
+import { Form, DropdownButton } from 'react-bootstrap';
 import { SelectOptions } from '../services/options';
 
-
-export const handleGetName = (array:number[]) => {
-  const selectedOption = new Set(array)
-  return SelectOptions.filter((option) => selectedOption.has(option.id)).map(
-      option => option.name
-  )
+interface IMultiSelect {
+  selectedOptions: string[];
+  setSelectedOptions: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const MultiSelect = () => {
-   const {setSelectedOptions} = useContext(AppContext)
-
-   const handleOptions = (optionId: number) => {
-    const updatedOptions = SelectOptions.map((option) => {
-        if(option.id === optionId){
-            return{ ...option, selected:!option.selected }
-        } else {
-            return option
-        }
-    });
-     setSelectedOptions((prevSelectedOptions) => {
-        if(prevSelectedOptions.includes(optionId)){
-          return prevSelectedOptions.filter((id) => id !== optionId)
-        }
-        else { 
-            return [...prevSelectedOptions, optionId]
-        }
-     });
-
-     const updatedSelectOptions = [...updatedOptions];
-     SelectOptions.forEach((option, index) => {
-    Object.assign(option, updatedSelectOptions[index]);
-  });
-     
+const MultiSelect = ({ selectedOptions, setSelectedOptions }: IMultiSelect) => {
+  
+  const handleOptions = (option: string) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((o) => o !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
     }
-    
-   
+  };
 
-    return(
-     <Form.Group className='d-flex justify-content-center p-3'>
-        <Form.Label className='mt-2 mx-3 '>
-            مهارت ها:
-        </Form.Label>
-        <DropdownButton
-         variant='light'
-         drop='down-centered'
-         title="مهارت ها">
-          {
-          SelectOptions.map((option) => (
-              <div key={option.id} className='d-flex d-flex justify-content-end mx-auto'> 
-                <Form.Label htmlFor={`option ${option.id}`}>
-                 {option.name} 
-                </Form.Label>
-                <Form.Check
-                 id={`option ${option.id}`}
-                 className='px-2'
-                 type='checkbox'
-                 checked={option.selected}
-                 onChange={() => handleOptions(option.id)} 
-                 />    
-              </div> 
-            ))}
-        </DropdownButton>
-     </Form.Group>
-    )
-}
+  return (
+    <Form.Group className='d-flex justify-content-center p-3'>
+      <Form.Label className='mt-2 mx-3'>مهارت ها:</Form.Label>
+      <DropdownButton title='مهارت ها' drop='down-centered' variant='light'>
+        {SelectOptions.map((option) => (
+          <div key={option} className='d-flex justify-content-center px-1 my-2'>
+            <Form.Label>{option}</Form.Label>
+            <Form.Check
+              defaultChecked={selectedOptions.includes(option)}
+              className='me-1'
+              type='checkbox'
+              onClick={() => handleOptions(option)}
+            />
+          </div>
+        ))}
+      </DropdownButton>
+    </Form.Group>
+  );
+};
 
 export default MultiSelect;

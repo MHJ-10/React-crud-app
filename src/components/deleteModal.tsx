109eea1a44224bdba500084765f3139c1693ba4f
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-05-04 09:59:41 +0330

    fix bugs and edit modal title

--- src/components/deleteModal.tsx
+++ src/components/deleteModal.tsx
@@ -1,27 +1,37 @@
+import React, {useState} from "react";
 import { Modal, Button } from 'react-bootstrap';
+import { IUser } from '../hooks/stateContext';
 
-
-interface IDeleteModal{
-    show: boolean
-    handleClose: () => void
-    handleDelete: () => void
+interface IDelete {
+    userP: IUser
+    users: IUser[]
+    setUsers: React.Dispatch<React.SetStateAction<IUser[]>>
 }
-const DeleteModal = ({show, handleClose, handleDelete}: IDeleteModal) => {
-    return(
-        <div className="modal show">
-            <Modal show={show}>
-                
-                <Modal.Body>
-                 <h5>آیا از حدف این کاربر مطمئنید؟</h5>
-                </Modal.Body>
-                <Modal.Footer>
-                   <Button variant='danger' onClick={handleClose}>
-                     خیر
-                   </Button>
-                   <Button variant='success'  onClick={handleDelete}>
-                     بله
-                   </Button>
-                </Modal.Footer>
+
+const DeleteModal = ({userP, users, setUsers}: IDelete) => {
+ const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false)
+
+ const handleDelete = () => {
+   const filteredUsers = users.filter(u => u.id !== userP.id)
+   setUsers(filteredUsers)
+   localStorage.setItem("users", JSON.stringify(filteredUsers))
+   setShowDeleteModal(false)
+ }
+
+
+  return (
+        <div>
+            <Button variant="danger" onClick={() => setShowDeleteModal(true)}>
+                حذف
+            </Button>
+            <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
+             <Modal.Body>
+                 <h5>آیا از حذف {userP.fullName} مطمئنید؟</h5>
+             </Modal.Body>
+             <Modal.Footer>
+              <Button variant='danger' onClick={() => setShowDeleteModal(false)}> خیر </Button>
+              <Button variant='success' onClick={() => handleDelete()}> بله </Button>
+             </Modal.Footer>
             </Modal>
         </div>
     )
